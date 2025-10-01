364e68b5a4f7bd05ec87bf5bbee890df73b81e22
diabalsaadiOP <diab.alsaadi@optimumpartners.co>
2025-10-01 23:11:53 +0300

    wip

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -6,7 +6,7 @@ import { signOut } from "next-auth/react";
 
 const Navigation = () => {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const { isAuthenticated, user } = useAppContext();
+  const { isAuthenticated } = useAppContext();
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
